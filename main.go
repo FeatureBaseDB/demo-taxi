f5753583e797a2be1f83de09d15f544063715f4a
Matt Jaffee <jaffee@umbel.com>
2017-05-23 10:39:54 -0500

    gofmt and add glide

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-    "log"
-    "fmt"
-    "time"
-    "encoding/json"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gorilla/mux"
-	pilosa "github.com/pilosa/go-pilosa"	
+	pilosa "github.com/pilosa/go-pilosa"
 )
 
 const host = ":10101"
@@ -21,10 +22,10 @@ func main() {
 	server.Serve()
 }
 
-type Server struct{
+type Server struct {
 	Router *mux.Router
 	Client *pilosa.Client
-	Index *pilosa.Index
+	Index  *pilosa.Index
 	Frames map[string]*pilosa.Frame
 }
 
@@ -59,9 +60,9 @@ func NewServer() *Server {
 	_ = client.EnsureFrame(typeFrame)
 
 	frames := map[string]*pilosa.Frame{
-		"year": yearFrame,
-		"pcount": pcountFrame,
-		"dist": distFrame,
+		"year":    yearFrame,
+		"pcount":  pcountFrame,
+		"dist":    distFrame,
 		"cabtype": typeFrame,
 	}
 
@@ -80,8 +81,8 @@ func (s Server) testQuery() {
 	result := response.Result()
 	// Act on the result
 	if result != nil {
-	    bits := result.Bitmap.Bits
-	    fmt.Printf("Got bits: %v\n", bits)
+		bits := result.Bitmap.Bits
+		fmt.Printf("Got bits: %v\n", bits)
 	}
 }
 
@@ -91,15 +92,15 @@ func (s Server) Serve() {
 }
 
 func (s Server) HandlePredefined3(w http.ResponseWriter, r *http.Request) {
-    // NxM queries, N, M = cardinality of passenger_count (8), year (7) - medium priority
-	t := time.Now()	
+	// NxM queries, N, M = cardinality of passenger_count (8), year (7) - medium priority
+	t := time.Now()
 	numRides := s.getRideCount()
 	rows := make([]Predefined3Row, 40)
 	// queries go here
 	dif := time.Since(t)
 
 	err := json.NewEncoder(w).Encode(predefined3Response{
-		numRides, 
+		numRides,
 		"Profile count by (year, passenger_count) (Mark #3) (go)",
 		float64(dif.Seconds()),
 		rows,
@@ -112,9 +113,9 @@ func (s Server) HandlePredefined3(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s Server) HandlePredefined3Serial(w http.ResponseWriter, r *http.Request) {
-    // NxM queries, N, M = cardinality of passenger_count (8), year (7) - medium priority
+	// NxM queries, N, M = cardinality of passenger_count (8), year (7) - medium priority
 
-	t := time.Now()	
+	t := time.Now()
 	numRides := s.getRideCount()
 	rows := make([]Predefined3Row, 40)
 
@@ -134,7 +135,7 @@ func (s Server) HandlePredefined3Serial(w http.ResponseWriter, r *http.Request) 
 	dif := time.Since(t)
 
 	err := json.NewEncoder(w).Encode(predefined3Response{
-		numRides, 
+		numRides,
 		"Profile count by (year, passenger_count) (Mark #3) (go)",
 		float64(dif.Seconds()),
 		rows,
@@ -147,35 +148,33 @@ func (s Server) HandlePredefined3Serial(w http.ResponseWriter, r *http.Request) 
 }
 
 type predefined3Response struct {
-	NumRides uint64 `json:"numProfiles"`
-	Description string `json:"description"`
-	Seconds float64 `json:"seconds"`
-	Rows []Predefined3Row `json:"rows"`
+	NumRides    uint64           `json:"numProfiles"`
+	Description string           `json:"description"`
+	Seconds     float64          `json:"seconds"`
+	Rows        []Predefined3Row `json:"rows"`
 }
 
 type Predefined3Row struct {
-	Count uint64 `json:"count"`
-	Year int `json:"year"`
-	PassengerCount int `json:"passenger_count"`
+	Count          uint64 `json:"count"`
+	Year           int    `json:"year"`
+	PassengerCount int    `json:"passenger_count"`
 }
 
 func (s Server) HandlePredefined4(w http.ResponseWriter, r *http.Request) {
-    // NxMxP queries, N, M, P = cardinality of passenger_count (8), year (7), dist_miles (high) - high priority
-	t := time.Now()	
+	// NxMxP queries, N, M, P = cardinality of passenger_count (8), year (7), dist_miles (high) - high priority
+	t := time.Now()
 	numRides := s.getRideCount()
 	rows := make([]Predefined4Row, 100)
 
 	// queries go here
 
-
-
 	dif := time.Since(t)
 
 	err := json.NewEncoder(w).Encode(predefined4Response{
-		numRides, 
+		numRides,
 		"Profile count by (year, passenger_count, trip_distsance), ordered by (year, count) (Mark #4) (go)",
-		float64(dif.Seconds()), 
-		percentThreshold, 
+		float64(dif.Seconds()),
+		percentThreshold,
 		rows,
 	})
 
@@ -186,18 +185,18 @@ func (s Server) HandlePredefined4(w http.ResponseWriter, r *http.Request) {
 }
 
 type predefined4Response struct {
-	NumRides uint64 `json:"numProfiles"`
-	Description string `json:"description"`
-	Seconds float64 `json:"seconds"`
-	Threshold float64 `json:"percentageThreshold"`
-	Rows []Predefined4Row `json:"rows"`
+	NumRides    uint64           `json:"numProfiles"`
+	Description string           `json:"description"`
+	Seconds     float64          `json:"seconds"`
+	Threshold   float64          `json:"percentageThreshold"`
+	Rows        []Predefined4Row `json:"rows"`
 }
 
 type Predefined4Row struct {
-	Count uint64 `json:"count"`
-	Distance int `json:"distance"`
-	PassengerCount int `json:"passenger_count"`
-	PickupYear int `json:"pickup_year"`
+	Count          uint64 `json:"count"`
+	Distance       int    `json:"distance"`
+	PassengerCount int    `json:"passenger_count"`
+	PickupYear     int    `json:"pickup_year"`
 }
 
 func (s Server) getRideCount() uint64 {
@@ -205,30 +204,30 @@ func (s Server) getRideCount() uint64 {
 	for n := 0; n < 10; n++ {
 		response, _ := s.Client.Query(s.Index.Count(s.Frames["cabtype"].Bitmap(uint64(n))), nil)
 		count += response.Result().Count
-	}	
+	}
 	return count
 }
 
 func HandleFrontend(w http.ResponseWriter, r *http.Request) {
-    // static - fine in python
+	// static - fine in python
 }
 
 func HandleIntersect(w http.ResponseWriter, r *http.Request) {
-    // only runs one query - fine in python
+	// only runs one query - fine in python
 }
 
 func HandleTopN(w http.ResponseWriter, r *http.Request) {
-    // only runs one query - fine in python
+	// only runs one query - fine in python
 }
 
 func HandlePredefined1(w http.ResponseWriter, r *http.Request) {
-    // N queries, N = cardinality of cab_type (3) - lowest priority
+	// N queries, N = cardinality of cab_type (3) - lowest priority
 }
 
 func HandlePredefined2(w http.ResponseWriter, r *http.Request) {
-    // N queries, N = cardinality of passenger_count (8) - low priority
+	// N queries, N = cardinality of passenger_count (8) - low priority
 }
 
 func HandlePredefined5(w http.ResponseWriter, r *http.Request) {
-  // 2 queries - lowest priority
+	// 2 queries - lowest priority
 }
